6953732fe744a0d766cf85d3ca1be7865fb288b9
LuisEC10 <luisicpna10@gmail.com>
2026-01-08 19:08:26 -0500

    interacting with contracts ABI

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage(); // solidity know to deploy a contract
+        SimpleStorage newSimpleStorage = new SimpleStorage(); // solidity know to deploy a contract
+        listOfSimpleStorageContracts.push(newSimpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI -> Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
